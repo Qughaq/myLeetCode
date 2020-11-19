e005302d68703f11e263a591ecdc5612c60462f4
Qughaq <965003795@qq.com>
2020-11-19 12:11:59 +0800

    compiler

--- LinkedList/src/MySinglyLinkedList.java
+++ LinkedList/src/MySinglyLinkedList.java
@@ -27,7 +27,7 @@ class MySinglyLinkedList {
      * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
      */
     public int get(int index) {
-        if (index >= size || index < 0)
+        if ( index >= size || index < 0 )
             return -1;
         Node cur = head;
         for (int i = 0; i <= index; i++)
@@ -44,7 +44,7 @@ class MySinglyLinkedList {
                 Node(val);
         head.next.next = temp;
         size++;//计数
-        if (size == 1)
+        if ( size == 1 )
             tail = head.next;
     }
 
@@ -62,7 +62,7 @@ class MySinglyLinkedList {
      * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
      */
     public void addAtIndex(int index, int val) {
-        if (index > size)
+        if ( index > size )
             return;
         Node cur = head;
         for (int i = 0; i < index; i++)
@@ -70,7 +70,7 @@ class MySinglyLinkedList {
         Node temp = cur.next;
         cur.next = new
                 Node(val);
-        if (index == size)
+        if ( index == size )
             tail = cur.next;
         cur.next.next = temp;
         size++;
@@ -80,14 +80,14 @@ class MySinglyLinkedList {
      * Delete the index-th node in the linked list, if the index is valid.
      */
     public void deleteAtIndex(int index) {
-        if (index >= size || index < 0)
+        if ( index >= size || index < 0 )
             return;
         Node cur = head;
         for (int i = 0; i < index; i++)
             cur = cur.next;
         cur.next = cur.next.next;
         size--;
-        if (cur.next == null)
+        if ( cur.next == null )
             tail = cur;
     }
 }
