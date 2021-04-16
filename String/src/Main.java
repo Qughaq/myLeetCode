78176acd15fac7e251bbdb9b107a7217c3318043
Qughaq <965003795@qq.com>
2021-04-16 22:25:09 +0800

    2021/04/16

--- String/src/Main.java
+++ String/src/Main.java
@@ -1,7 +1,8 @@
 // "static void main" must be defined in a public class.
 public class Main {
     public static void main(String[] args) {
-        String ab = "Ssjbdhbhcdf";
-        char[] chars = ab.toCharArray();
+        String s1 = "great", s2 = "wwwas";
+        IsScramble87 isScramble87 = new IsScramble87();
+        System.out.println(isScramble87.isScramble(s1, s2));
     }
 }
