74aa30f96b314fbbc26cd988b4ad0c0ed07a2a18
Qughaq <965003795@qq.com>
2021-04-25 09:33:47 +0800

    2021-04-25

--- BinaryTree/src/BinaryTreeSolution.java
+++ BinaryTree/src/BinaryTreeSolution.java
@@ -6,6 +6,26 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTreeSolution {
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+
+        TreeNode dummyNode = new TreeNode(-1);
+        TreeNode currNode = dummyNode;
+        for (int value : res) {
+            currNode.right = new TreeNode(value);
+            currNode = currNode.right;
+        }
+        return dummyNode.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) return;
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
